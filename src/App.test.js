a8885d0f4dce75fe2f88f7bcce8b63163053667b
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-07-27 11:50:40 +0900

    eslint 설정

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,6 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable no-undef */
+/* eslint-disable prettier/prettier */
 import { render, screen } from '@testing-library/react';
 import App from './App';
 

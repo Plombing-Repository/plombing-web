7a1644db2aeb5ba2e3c340b8ea3753797e2041fc
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-08-06 14:08:48 +0900

    [feat] Footer 추가

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 import { ReactComponent as Polygon } from './assets/polygon.svg';
 import { ReactComponent as LastPhaseModel } from './assets/last_phase.svg';
+import Footer from './Footer';
 
 const Home = () => {
   const [progress, setProgress] = useState(0);
@@ -21,32 +22,35 @@ const Home = () => {
   }, [percentage]);
 
   return (
-    <Section>
-      <Banner>
-        <BannerText>
-          <h1>
-            운동하며 환경보호까지!
-            <br />
-            마운틴 플로깅, 플로밍
-          </h1>
-          <h3> 지금까지 341명이 플로밍했어요</h3>
-          <Progress style={{ width: `${progress + 20}px` }}>
-            <p>{percentage}%</p>
-            <Polygon style={{ marginRight: '15px' }} />
-          </Progress>
-          <div className="progress-container">
-            <div
-              className="progress-bar"
-              style={{ width: `${progress}px`, background: '#80d088' }}
-            ></div>
-          </div>
-          <button onClick={() => window.location.replace('/flombing')}>
-            플로밍 하기
-          </button>
-        </BannerText>
-        <LastPhaseModel />
-      </Banner>
-    </Section>
+    <>
+      <Section>
+        <Banner>
+          <BannerText>
+            <h1>
+              운동하며 환경보호까지!
+              <br />
+              마운틴 플로깅, 플로밍
+            </h1>
+            <h3> 지금까지 341명이 플로밍했어요</h3>
+            <Progress style={{ width: `${progress + 20}px` }}>
+              <p>{percentage}%</p>
+              <Polygon style={{ marginRight: '15px' }} />
+            </Progress>
+            <div className="progress-container">
+              <div
+                className="progress-bar"
+                style={{ width: `${progress}px`, background: '#80d088' }}
+              ></div>
+            </div>
+            <button onClick={() => window.location.replace('/flombing')}>
+              플로밍 하기
+            </button>
+          </BannerText>
+          <LastPhaseModel />
+        </Banner>
+      </Section>
+      <Footer />
+    </>
   );
 };
 
@@ -57,16 +61,21 @@ const Section = styled.div`
   background-repeat: no-repeat;
   width: 100%;
   height: 100vh;
+  padding: 40px 240px;
+  box-sizing: border-box;
   z-index: -1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const Banner = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  width: fit-content;
+  width: 100%;
   justify-content: center;
-  padding: 40px 240px;
+  margin-left: 24px;
 `;
 
 const BannerText = styled.div`
@@ -109,7 +118,7 @@ const BannerText = styled.div`
     border-radius: 16px;
     border: none;
     letter-spacing: -0.5px;
-    margin-top: 50px;
+    margin-top: 52px;
     &:hover {
       cursor: pointer;
     }
@@ -122,10 +131,10 @@ const Progress = styled.div`
   justify-content: flex-end;
   align-items: flex-end;
   position: relative;
-  bottom: 10px;
+  bottom: 8px;
   transition: all 2s ease-out;
   p {
-    font-size: 21px;
+    font-size: 20px;
     margin: 0;
     height: 32px;
     cursor: default;

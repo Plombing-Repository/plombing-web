9025e7dbc0aca380da158559ef99c8e8a204b863
kimhongrae <khr0690@gmail.com>
2023-08-06 14:24:48 +0900

    [feat]박스 내 버튼 구현

---
+++ src/pages/Participation/CounterButton.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  border-radius: 100%;
+  background-color: #ffffff;
+  border-color: #ffffff;
+`;
+
+const CounterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: white;
+  border: 1px solid #ddd;
+  border-radius: 799.139px;
+  box-shadow: 0px -4.508937835693359px 22.544689178466797px rgba(0, 0, 0, 0.1);
+  padding: 10px;
+  width: 210px;
+  height: 20px;
+  flex-shrink: 0;
+  margin: auto;
+`;
+
+const CounterButton = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <CounterContainer>
+      <StyledButton onClick={() => setCount(count - 1)}>-</StyledButton>
+      <span>{count}</span>
+      <StyledButton onClick={() => setCount(count + 1)}>+</StyledButton>
+    </CounterContainer>
+  );
+};
+
+export default CounterButton;

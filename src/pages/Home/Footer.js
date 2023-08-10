0766021057c5e1c771e12b60daa68b86f8744df5
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-08-10 18:23:20 +0900

    [feat] 화면 구성

--- src/pages/Home/Footer.js
+++ src/pages/Home/Footer.js
@@ -5,10 +5,12 @@ const Footer = () => {
   return (
     <Section>
       <TextBox>
-        <h3>환경을 생각하는 인공폭포</h3>
+        <h3>필(必)환경을 부르는 &apos;글로벌 보일링&apos; 시대</h3>
         <p>
-          삭막한 도시 속에서 시원한 폭포를 바라볼 수 있는 <br />
-          휴식 공간이 인기를 끌고 있습니다.
+          지구가 끓고 있다. 지구가 내는 신음을 가벼이 여겨서는 안 된다. 유엔
+          사무총장은 27일(현지시간) <br />
+          &quot;지구 &apos;온난화&apos; 시대는 끝났고 지구가 &apos;끓는&apos;
+          시대가 도래했다&quot; 말했다.
         </p>
       </TextBox>
       <button>read more</button>
@@ -26,6 +28,8 @@ const Section = styled.section`
   align-items: flex-start;
   background-image: url('https://velog.velcdn.com/images/ea_st_ring/post/915c841c-f51c-47bd-95e2-88e50b5e7b71/image.png');
   padding: 60px 240px 40px 240px;
+  position: absolute;
+  bottom: -1200px;
   button {
     width: 100px;
     height: 32px;
@@ -54,6 +58,9 @@ const TextBox = styled.div`
     font-weight: 600;
     margin-bottom: 1rem;
     margin-top: 0;
+  }
+  p {
+    line-height: 150%;
   }
 `;
 

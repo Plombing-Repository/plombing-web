d35e05a1f44fb997664328f8f23631456ec1abed
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-08-17 16:32:23 +0900

    [feat] 새로고침 시 최상단 이동 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { createGlobalStyle, styled } from 'styled-components';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Community from './pages/Community/Community';
 import About from './pages/About/About';
+import Articles from './pages/Article/Articles';
 
 function App() {
+  // 새로고침 시 최상단 이동
+  useEffect(() => {
+    window.onbeforeunload = function pushRefresh() {
+      window.scrollTo(0, 0);
+    };
+  }, []);
+
   return (
     <Container>
       <GlobalStyle />
@@ -14,6 +22,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/community" element={<Community />}></Route>
         <Route path="/about" element={<About />}></Route>
+        <Route path="/article/:id" element={<Articles />} />
       </Routes>
     </Container>
   );

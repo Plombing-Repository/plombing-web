06d2e924c650352d1655e04fd593722fcb272128
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-08-17 16:32:40 +0900

    [feat] Article 화면 구현

---
+++ src/pages/Article/Article2.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Format from './Format';
+import articleInfo from './articles.json';
+import { styled } from 'styled-components';
+
+const Article2 = () => {
+  const info = articleInfo.articles[1];
+  const Content = () => (
+    <Wrapper>
+      {info.content1.split('\n').map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
+    </Wrapper>
+  );
+  return (
+    <div>
+      <Format
+        id={info.id}
+        background={info.background}
+        title={info.title}
+        info={`${info.date} by ${info.writer}`}
+        content={<Content />}
+        source={info.source}
+      />
+    </div>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+
+  img {
+    object-fit: contain;
+    width: 100%;
+  }
+`;
+
+export default Article2;

d9a8347092dae1b0ea365a9e7981b77e2fb27361
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-08-19 21:08:36 +0900

    [chore] 로컬 동기화

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { createGlobalStyle, styled } from 'styled-components';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Community from './pages/Community/Community';
-import Header from './pages/Home/Header';
-import Participation from './pages/Participation/Participation';
+import About from './pages/About/About';
+import Articles from './pages/Article/Articles';
+import Participation from './pages/Participate/Participation';
 
 function App() {
+  // 새로고침 시 최상단 이동
+  useEffect(() => {
+    window.onbeforeunload = function pushRefresh() {
+      window.scrollTo(0, 0);
+    };
+  }, []);
+
   return (
-    <>
-      <Header />
+    <Container>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/community" element={<Community />}></Route>
-        <Route path="/flombing" element={<Participation />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/article/:id" element={<Articles />} />
+        <Route path="plombing" element={<Participation />} />
       </Routes>
-    </>
+    </Container>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  *::selection{
+    background-color: #a3cca2;
+    color: black;
+  }
+  body {
+    font-family: Pretendard;
+  }
+`;
+
+const Container = styled.div`
+  position: relative;
+`;

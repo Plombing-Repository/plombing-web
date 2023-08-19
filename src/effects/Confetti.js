69bd045e1965dbd037ab840bf8fa2142a72e5ecd
Ea-st-ring <95581482+Ea-st-ring@users.noreply.github.com>
2023-08-19 20:55:33 +0900

    [style] 팡파레 양 추가

--- src/effects/Confetti.js
+++ src/effects/Confetti.js
@@ -13,13 +13,46 @@ export default function Confetti() {
     refAnimationInstance.current &&
       refAnimationInstance.current({
         ...opts,
-        origin: { y: 0.7 },
+        origin: { y: 0.8 },
         particleCount: Math.floor(300 * particleRatio),
         gravity: 3.5,
       });
   }, []);
 
-  useEffect(() => fire(), []);
+  const makeShot2 = useCallback((particleRatio, opts) => {
+    refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { x: 0.2, y: 0.6 },
+        particleCount: Math.floor(200 * particleRatio),
+        gravity: 3.5,
+      });
+  }, []);
+
+  const makeShot3 = useCallback((particleRatio, opts) => {
+    refAnimationInstance.current &&
+      refAnimationInstance.current({
+        ...opts,
+        origin: { x: 0.7, y: 0.5 },
+        particleCount: Math.floor(200 * particleRatio),
+        gravity: 3.5,
+      });
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      fire();
+    }, 500);
+    setTimeout(() => {
+      fire2();
+    }, 1000);
+    setTimeout(() => {
+      fire3();
+    }, 1200);
+    setTimeout(() => {
+      fire();
+    }, 1500);
+  }, []);
 
   const fire = useCallback(() => {
     makeShot(0.25, {
@@ -57,6 +90,52 @@ export default function Confetti() {
       startVelocity: 45,
     });
   }, [makeShot]);
+
+  const fire2 = useCallback(() => {
+    makeShot2(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+
+    makeShot2(0.2, {
+      spread: 60,
+    });
+
+    makeShot2(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+
+    makeShot2(0.1, {
+      spread: 180,
+      startVelocity: 45,
+    });
+  }, [makeShot2]);
+
+  const fire3 = useCallback(() => {
+    makeShot3(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+
+    makeShot3(0.2, {
+      spread: 60,
+    });
+
+    makeShot3(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+
+    makeShot3(0.1, {
+      spread: 180,
+      startVelocity: 45,
+    });
+  }, [makeShot3]);
 
   return (
     <ReactCanvasConfetti
